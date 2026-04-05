d2d1719c0765f7b8e6e152a28ecd5ffff2b94ed9
csmccreery <cameron@mccreer.dev>
2026-04-05 12:45:48 -0400

    initial commit

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+	description = "Pyrite is a portable and composable tool for building static websites from a variety of markup languages";
+	inputs = {
+		nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+	};
+
+	outputs = { self, nixpkgs, ... }@inputs:
+	let
+		system = "x86_64-linux";
+		pkgs = import nixpkgs { inherit system; };
+	in {
+		devShells.${system}.default = pkgs.mkShell {
+			buildInputs = [
+				pkgs.python315
+                pkgs.pyright
+				pkgs.bootdev-cli
+				pkgs.docker
+				pkgs.uv
+			];
+		};
+	};
+}
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from pyrite!")
+
+
+if __name__ == "__main__":
+    main()
